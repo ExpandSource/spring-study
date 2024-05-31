c11e355ce7464c7c7b5936ef3c168e48cc33e973
expandsource <expandsource@gmail.com>
2024-05-31 14:53:49 +0900

    테이블 다대일 관계(양방향) 엔티티 매핑, DTO 추가 리팩토링

---
+++ src/main/java/com/busanit/spring_study/article/ArticleController.java
@@ -0,0 +1,56 @@
+package com.busanit.spring_study.article;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/articles")
+@RequiredArgsConstructor
+public class ArticleController {
+
+    //@Autowired
+    final private ArticleService articleService;
+
+    @PostMapping
+    public ResponseEntity<ArticleDTO> createArticle(@RequestBody ArticleDTO article) {
+        ArticleDTO createdArticle = articleService.createArticle(article);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdArticle);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ArticleDTO>> getAllArticles() {
+        List<ArticleDTO> articles = articleService.getAllArticles();
+        return ResponseEntity.ok(articles);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArticleDTO> getArticleById(@PathVariable Long id) {
+        ArticleDTO article = articleService.getArticleById(id);
+        if (article == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(article);
+    }
+
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ArticleDTO> updateArticle(@PathVariable Long id, @RequestBody ArticleDTO updatedArticle) {
+        ArticleDTO article = articleService.updateArticle(id, updatedArticle);
+        if (article == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(article);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable Long id) {
+        if (!articleService.deleteArticle(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+}
